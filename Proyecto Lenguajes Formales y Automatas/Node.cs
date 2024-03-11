2f3c5cfefb0c8d6b8c10c099486182571994b04f
CESAR ALESSANDRO MEJIA PEREZ <camejiap@correo.url.edu.gt>
2024-03-11 17:14:25 -0600

    Se crea el nodo de la estructura de AFD

--- Proyecto Lenguajes Formales y Automatas/Node.cs
+++ Proyecto Lenguajes Formales y Automatas/Node.cs
@@ -8,5 +8,17 @@ namespace Proyecto_Lenguajes_Formales_y_Automatas
 {
     internal class Node
     {
+        private string Sname;
+        private List<(string Strans_name, string Ssymbol)> Ltransitions;
+        private Boolean Bfinal_state;
+
+        public Node(string name, List<(string, string)> transitions, Boolean finalstate)
+        {
+            this.Sname = name;
+            this.Ltransitions = transitions;
+            this.Bfinal_state = finalstate;
+        }
+
+        public Node() { }
     }
 }
