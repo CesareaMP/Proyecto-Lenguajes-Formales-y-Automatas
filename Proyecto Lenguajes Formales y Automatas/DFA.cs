70a997d8e593af8d8bb2fe742f6685c9adc7adae
CESAR ALESSANDRO MEJIA PEREZ <camejiap@correo.url.edu.gt>
2024-03-12 09:02:32 -0600

    Se cambio el metodo Higher por Compare para comparar alfabeticamente

--- Proyecto Lenguajes Formales y Automatas/DFA.cs
+++ Proyecto Lenguajes Formales y Automatas/DFA.cs
@@ -16,27 +16,27 @@ namespace Proyecto_Lenguajes_Formales_y_Automatas
 
         public List<Node> MergeSort(List<Node> nodes)
         {
-            if (nodes.Count==1)
+            if (nodes.Count == 1)
             {
                 return nodes;
             }
-            int half = (int)Math.Round(nodes.Count/2.0);
+            int half = (int)Math.Round(nodes.Count / 2.0);
             List<Node> Lsplited_list1 = nodes.GetRange(0, half);
             List<Node> Lsplited_list2 = nodes.GetRange(half, (nodes.Count - half));
 
             Lsplited_list1 = MergeSort(Lsplited_list1);
             Lsplited_list2 = MergeSort(Lsplited_list2);
 
-            return Merge(Lsplited_list1,Lsplited_list2);
+            return Merge(Lsplited_list1, Lsplited_list2);
         }
 
         private List<Node> Merge(List<Node> Lsplited_list1, List<Node> Lsplited_list2)
         {
             List<Node> MergedList = new List<Node>();
 
-            while (Lsplited_list1.Count != 0 && Lsplited_list2.Count != 0){
-                if (Higher(Lsplited_list1[0].GetSname(), Lsplited_list2[0].GetSname()))
-                {                    
+            while (Lsplited_list1.Count != 0 && Lsplited_list2.Count != 0) {
+                if (Compare(Lsplited_list1[0].GetSname(), Lsplited_list2[0].GetSname())>0)
+                {
                     MergedList.Add(Lsplited_list2[0]);
                     Lsplited_list2.RemoveAt(0);
                 }
@@ -52,7 +52,7 @@ namespace Proyecto_Lenguajes_Formales_y_Automatas
                 MergedList.Add(Lsplited_list1[0]);
                 Lsplited_list1.RemoveAt(0);
             }
-            
+
             while (Lsplited_list2.Count != 0)
             {
                 MergedList.Add(Lsplited_list2[0]);
@@ -60,11 +60,6 @@ namespace Proyecto_Lenguajes_Formales_y_Automatas
             }
 
             return MergedList;
-        }
-
-        private bool Higher(string key1, string key2)
-        {
-            return key1.CompareTo(key2) > 0;
         }
 
         public int BinarySearch(List<Node> nodes, string key)
@@ -80,7 +75,7 @@ namespace Proyecto_Lenguajes_Formales_y_Automatas
             while (low <= high)
             {
                 int mid = (low + high) / 2;
-                int comparisonResult = string.Compare(key, nodes[mid].GetSname());
+                int comparisonResult = Compare(key, nodes[mid].GetSname());
 
                 if (comparisonResult == 0)
                 {
@@ -98,6 +93,34 @@ namespace Proyecto_Lenguajes_Formales_y_Automatas
             return -1;
         }
 
+        private int Compare(string x, string y)
+        {
+            // Parse numbers from strings
+            string[] xParts = System.Text.RegularExpressions.Regex.Split(x, @"(\d+)");
+            string[] yParts = System.Text.RegularExpressions.Regex.Split(y, @"(\d+)");
+
+            for (int i = 0; i < Math.Min(xParts.Length, yParts.Length); i++)
+            {
+                // If both parts are numeric, parse and compare numerically
+                int xNum, yNum;
+                if (int.TryParse(xParts[i], out xNum) && int.TryParse(yParts[i], out yNum))
+                {
+                    int comparison = xNum.CompareTo(yNum);
+                    if (comparison != 0)
+                        return comparison;
+                }
+                else
+                {
+                    // If either part is not numeric, compare lexically
+                    int comparison = xParts[i].CompareTo(yParts[i]);
+                    if (comparison != 0)
+                        return comparison;
+                }
+            }
+
+            // If all parts are equal, compare the lengths
+            return x.Length.CompareTo(y.Length);
+        }
 
 
     }
